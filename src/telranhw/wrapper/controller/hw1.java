7f140a8d486255c7c0f2a71fb13c8fb12ad1b4a8
YourMirror <kamenka993@gmail.com>
2022-10-31 19:32:48 +0200

    copy paste deleted array added

--- src/telranhw/wrapper/controller/hw1.java
+++ src/telranhw/wrapper/controller/hw1.java
@@ -4,22 +4,14 @@ public class hw1 {
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			System.out.println("Max byte: "+ Byte.MAX_VALUE);
-			System.out.println("Min byte: " + Byte.MIN_VALUE);
-			System.out.println("Max Integer: "+ Integer.MAX_VALUE);
-			System.out.println("Min Integer: " + Integer.MIN_VALUE);
-			System.out.println("Max Short: "+ Short.MAX_VALUE);
-			System.out.println("Min Short: " + Short.MIN_VALUE);
-			System.out.println("Max Long: "+ Long.MAX_VALUE);
-			System.out.println("Min Long: " + Long.MIN_VALUE);
-			System.out.println("Max Character: "+ +Character.MAX_VALUE);
-			System.out.println("Min Character: " + +Character.MIN_VALUE);
-			System.out.println("Max Float: "+ Float.MAX_VALUE);
-			System.out.println("Min Float: " + Float.MIN_VALUE);
-			System.out.println("Max Double: "+ Double.MAX_VALUE);
-			System.out.println("Min Double: " + Double.MIN_VALUE);
+			String[] arr = new String[] {"byte", "int", "short", "long", "char", "float", "double"};
+			for (int i = 0; i < arr.length; i++) {
+				System.out.println(arr[i]);
+				checker(arr[i]);
+				}
 		}else {
 		for (int i = 0; i < args.length; i++) {
+			
 			checker(args[i]);
 			}
 		}
